5593b92e7ac7171f0ba7cac510357959d8e04db3
arnob635 <arnob635@gmail.com>
2017-11-19 02:49:59 +0600

    debugging

--- register.php
+++ register.php
@@ -5,14 +5,16 @@ $password = $_POST["password"];
 //$cpassword =$_POST["cpassword"];
 
 $con=mysqli_connect($host,$user,$pass,$db);
-if($con)
-echo "connected";
 
-$sql = "insert into accounts (email,password) values ($email,$password)";
+
+$sql = "INSERT INTO accounts (email,password) values ($email,$password)";
 
 $query= mysqli_query($con,$sql);
 
 if($query)
 	echo " insert succesful ";
+
+else
+	echo "Error";
 
 ?>
